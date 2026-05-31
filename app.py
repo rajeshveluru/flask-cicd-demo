69c6858852a8467e6953254d67178966d943462c
Ubuntu <ubuntu@ip-172-31-21-95.ec2.internal>
2026-05-31 15:22:40 +0000

    rajesh chowdary

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD Demo!"
+    return "Rajesh chowdary DevOps engineer earnes 100k+!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
